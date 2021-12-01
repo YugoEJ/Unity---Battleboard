001c524023275524b9cb4c91a70613d7c964feed
Eliyahu Yakobov <yugoej@gmail.com>
2021-12-01 21:59:14 +0200

    Added Board (Matrix) Tiles
    - Added connected tiles

---
+++ Assets/Scripts/RouteScript.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RouteScript : MonoBehaviour
+{
+    Transform[] childObjects;
+    public List<Transform> childObjectList = new List<Transform>();
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+
+        FillNodes();
+
+        for (int i = 0; i < childObjectList.Count; i++)
+        {
+            Vector3 currentPos = childObjectList[i].position;
+
+            if (i > 0)
+            {
+                Vector3 prevPos = childObjectList[i - 1].position;
+                Gizmos.DrawLine(prevPos, currentPos);
+            }
+        }
+    }
+
+    void FillNodes()
+    {
+        childObjectList.Clear();
+
+        childObjects = GetComponentsInChildren<Transform>();
+
+        foreach (Transform child in childObjects)
+        {
+            if (child != this.transform)
+            {
+                childObjectList.Add(child);
+            }
+        }
+    }
+
+}
