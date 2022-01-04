98d5bfe294feb55a2d5643c8750b5676e0f28a6f
OmriMashiah <omrimashiahofficial@gmail.com>
2022-01-04 17:37:17 +0200

    Dodge-a-bullet-minigame

---
+++ Assets/Scripts/Dodge_a_bullet_minigame/ComputerMove.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComputerMove : MonoBehaviour
+{
+    public float timer;
+
+    public int newtarget;
+
+    public float speed;
+
+    public Vector3 target;
+
+
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
