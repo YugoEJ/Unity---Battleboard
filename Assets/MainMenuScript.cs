662a0b81ee4c90c4ab196f8b166542b19e880c46
Eliyahu Yakobov <yugoej@gmail.com>
2022-02-18 19:13:20 +0200

    Added Instructions menu
    - Instructions button leads to a page full of instructions
    - said page includes 3 buttons as well (start, back [to main menu], exit)

--- Assets/MainMenuScript.cs
+++ Assets/MainMenuScript.cs
@@ -5,14 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuScript : MonoBehaviour
 {
+    private int InstructionsScene = 0;
+    private int GameScene = 1;
+
     public void StartGame()
     {
-        SceneManager.LoadScene(2);
+        SceneManager.LoadScene(GameScene);
     }
 
     public void Instructions()
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(InstructionsScene);
     }
 
     public void ExitGame()
